ec7fbbed3556ac82ccbcf65cd04ee82e0fa341c4
shalini-9393 <shalinithatikonda73@gmail.com>
2026-03-24 21:05:02 +0530

    Create 01-common.sh

---
+++ 01-common.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+USERid=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$(basename $0).log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.shalinidaws88s.online
+
+mkdir -p "$LOGS_FOLDER"
+
+echo "$(date "+%Y-%m-%d %H:%M:%S") | Script started executing at: $(date)" | tee -a "$LOGS_FILE"
+
+check_root(){
+    if [ $USERid -ne 0 ]; then
+        echo -e "${R}Please run this script as root or with sudo.${N}" | tee -a "$LOGS_FILE"
+        exit 1
+    fi
+}
+
+check_root
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | ${R}${2} failed.${N}" | tee -a "$LOGS_FILE"
+        exit 1
+    else
+        echo -e "$(date "+%Y-%m-%d %H:%M:%S") | ${G}${2} successful.${N}" | tee -a "$LOGS_FILE"
+    fi
+}
+ nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NodeJS Default version"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NodeJS 20"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "Install NodeJS"
+
+    npm install  &>>$LOGS_FILE
+    VALIDATE $? "Installing dependencies"
+ }
+ app_setup(){
+    id roboshop &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+      useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+      VALIDATE $? "Creating system user"
+    else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    fi
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip  &>>$LOGS_FILE
+    VALIDATE $? "Downloading $app_name code"
+
+    cd /app
+    VALIDATE $? "Moving to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "Uzip $app_name code"
+ }
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "Created systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $app_name &>>$LOGS_FILE
+    systemctl start $app_name
+    VALIDATE $? "Starting and enabling $app_name"
+}
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarting $app_name"
+}
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( END_TIME - START_TIME ))
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Script executed in: ${G}${TOTAL_TIME} seconds${N}" | tee -a "$LOGS_FILE"
+}
